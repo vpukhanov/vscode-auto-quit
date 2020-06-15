a9c5e672698cdb87890f17309542093a018d29e4
Vyacheslav Pukhanov <vyacheslav.pukhanov@gmail.com>
2020-06-15 18:02:21 +0300

    formatting

--- src/extension.ts
+++ src/extension.ts
@@ -3,15 +3,25 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
 	let windowCount = vscode.window ? 1 : 0;
 
-	const newWindowSubscription = vscode.commands.registerCommand('autoQuit.keybindings.newWindow', () => {
-		++windowCount;
-		vscode.commands.executeCommand('workbench.action.newWindow');
-	});
+	const newWindowSubscription = vscode.commands.registerCommand(
+		'autoQuit.keybindings.newWindow',
+		() => {
+			++windowCount;
+			vscode.commands.executeCommand('workbench.action.newWindow');
+		}
+	);
 
-	const closeWindowSubscription = vscode.commands.registerCommand('autoQuit.keybindings.closeWindow', () => {
-		--windowCount;
-		vscode.commands.executeCommand(windowCount > 0 ? 'workbench.action.closeWindow' : 'workbench.action.quit');
-	});
+	const closeWindowSubscription = vscode.commands.registerCommand(
+		'autoQuit.keybindings.closeWindow',
+		() => {
+			--windowCount;
+			vscode.commands.executeCommand(
+				windowCount > 0
+					? 'workbench.action.closeWindow'
+					: 'workbench.action.quit'
+			);
+		}
+	);
 
 	context.subscriptions.push(newWindowSubscription);
 	context.subscriptions.push(closeWindowSubscription);
